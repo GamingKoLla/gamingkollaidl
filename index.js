a9d241e82dc8a3635f3d104246f922048382f7f0
GamingKoLla <86671054+GamingKoLla@users.noreply.github.com>
2024-02-20 20:13:29 +0530

    index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ var username2 = process.env.username2;
 var password2 = process.env.password2;
 var shared_secret2 = process.env.shared2;
 
-var games2 = [271590, 730, 578080, 1938090, 12210, 2073850, 1786950, 218620, 1806040, 2302640, 1905950, 400040];  // Enter here AppIDs of the needed games
+var games2 = [271590, 730, 578080, 1938090, 12210, 2073850, 1786950, 872200, 429300, 1623660, 1997040, 1041320];  // Enter here AppIDs of the needed games
 var status2 = 7;  // 1 - online, 7 - invisible
 
 
