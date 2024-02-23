75add832f2d9a329bf7dd2ace617f0aa66255c42
GamingKoLla <86671054+GamingKoLla@users.noreply.github.com>
2024-02-23 23:51:05 +0530

    index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var password2 = process.env.password2;
 var shared_secret2 = process.env.shared2;
 
 var games2 = [294420, 271590, 730, 578080, 1938090, 12210, 2073850, 1928420, 218620, 2240000, 2487070];  // Enter here AppIDs of the needed games
-var status2 = 1;  // 1 - online, 7 - invisible
+var status2 = 7;  // 1 - online, 7 - invisible
 
 
 user2 = new steamUser();
